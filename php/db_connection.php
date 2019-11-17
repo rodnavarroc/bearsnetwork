cf82d54ef70c072f8e1a492058ce0a98cc712b62
Luis Guerrero <manuel_guerreroalns@uanl.edu.mx>
2019-11-16 18:17:44 -0600

    Bug login fixed: div has been centered

--- php/db_connection.php
+++ php/db_connection.php
@@ -10,6 +10,8 @@ $dbname="bearsnetwork";
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
 
+
+$conn->set_charset('utf8');
 //$con->close();
 
 ?>
